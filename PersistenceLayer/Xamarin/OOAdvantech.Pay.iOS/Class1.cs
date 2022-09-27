09ef040ae7d2d5c974910eeb739b95daa7d25940
Jim Liakos <Jim.Liakos.Msdn@hotmail.com>
2022-09-27 10:56:15 +0300

    jim

---
+++ PersistenceLayer/Xamarin/OOAdvantech.Pay.iOS/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace OOAdvantech.Pay.iOS
+{
+    public class Class1
+    {
+    }
+}
---
+++ PersistenceLayer/Xamarin/OOAdvantech.Pay/CardInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOAdvantech.Pay
+{
+    public class CardInfo
+    {
+        public string CardNumber { get; set; }
+        public string Expiry { get; set; }
+        public string Cvv { get; set; }
+    }
+}
