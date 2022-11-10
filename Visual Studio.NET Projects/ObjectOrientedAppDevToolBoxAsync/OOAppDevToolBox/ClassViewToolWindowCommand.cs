d44a6052498fe905428aedbe3d0303cfef78219a
Jim Liakos <Jim.Liakos.Msdn@hotmail.com>
2022-11-10 22:00:40 +0200

    jim

--- Visual Studio.NET Projects/ObjectOrientedAppDevToolBoxAsync/OOAppDevToolBox/ClassViewToolWindowCommand.cs
+++ Visual Studio.NET Projects/ObjectOrientedAppDevToolBoxAsync/OOAppDevToolBox/ClassViewToolWindowCommand.cs
@@ -17,7 +17,7 @@ namespace OOAppDevToolBox
         /// <summary>
         /// Command ID.
         /// </summary>
-        public const int CommandId = 0x0100;
+        public const int CommandId = 0x0100; 
 
         /// <summary>
         /// Command menu group (command set GUID).
