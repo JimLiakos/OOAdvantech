860be3661ae68b1f6b64763bcac7848c7c9de04c
Jim Liakos <Jim.Liakos.Msdn@hotmail.com>
2022-12-02 19:56:36 +0200

    jim

--- PersistenceLayer/Xamarin/OOAdvantech.Pay/PaymentPage.xaml.cs
+++ PersistenceLayer/Xamarin/OOAdvantech.Pay/PaymentPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using FinanceFacade;
+using OOAdvantech.Web;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,22 +18,73 @@ namespace OOAdvantech.Pay
         {
             InitializeComponent();
             this.BindingContext = new PaymentPageViewModel();
-
-
         }
-
+        public bool PaySucceeded;
+        IPayment Payment;
         internal void LoadPayment(IPayment payment)
         {
+            Payment= payment;
             var providerJson = payment.PaymentProviderJson;
             if (!string.IsNullOrWhiteSpace(providerJson))
             {
                 var orderCode = OOAdvantech.Json.JsonConvert.DeserializeObject<PaymentOrderResponse>(providerJson)?.orderCode;
                 this.PayWebView.Uri=$"https://demo.vivapayments.com/web/checkout?ref={orderCode}";
-            }
 
-            
+                this.PayWebView.Navigated+=PayWebView_Navigated;
+            }
         }
-       
+
+        private void PayWebView_Navigated(object sender, Web.NavigatedEventArgs e)
+        {
+            string url = e.Address;
+
+            var payment = Payment;
+
+           // url="https://demo.vivapayments.com/web/checkout/result?t=051e0d67-d54e-4f7c-8bad-23180a41757b&s=3288397036572604&lang=en-GB&eventId=0&eci=1";
+            if (url.IndexOf("vivapayments.com/web/checkout/result")!=-1)
+            {
+                if (GetCheckoutData(url, payment))
+                {
+                    PaySucceeded=true;
+                    DeviceApplication.Current.OnBackPressed();
+                }
+            }
+        }
+
+        private static bool GetCheckoutData(string url, IPayment payment)
+        {
+            int queryStartPos = url.IndexOf("?");
+            if (queryStartPos != -1)
+            {
+                string query = url.Substring(queryStartPos + 1);
+                if (!string.IsNullOrWhiteSpace(query))
+                {
+                    var parameters = System.Web.HttpUtility.ParseQueryString(query);
+                    if (parameters.Get("t")!=null)
+                    {
+                        PaymentOrderResponse paymentOrderResponse = OOAdvantech.Json.JsonConvert.DeserializeObject<PaymentOrderResponse>(payment.PaymentProviderJson);
+
+                        paymentOrderResponse.transactionId = parameters.Get("t");
+                        paymentOrderResponse.orderCode =long.Parse(parameters.Get("s"));
+                        paymentOrderResponse.eventId = parameters.Get("eventId");
+                        paymentOrderResponse.eci = parameters.Get("eci");
+                        return true;
+
+                    }
+                }
+            }
+            return false;
+        }
+
+        public class PaymentOrderResponse
+        {
+            internal string eventId;
+            internal string eci;
+
+            public long orderCode { get; set; }
+            public long expiring { get; set; }
+            public string transactionId { get; internal set; }
+        }
     }
 
     public class PaymentService
@@ -66,8 +118,6 @@ namespace OOAdvantech.Pay
             Xamarin.Forms.Device.BeginInvokeOnMainThread(async () =>
             {
                 PaymentPage=new PaymentPage();
-             
-
 
                 PaymentPage.LoadPayment(payment);
 
@@ -79,17 +129,15 @@ namespace OOAdvantech.Pay
 
 
         }
-        public class PaymentOrderResponse
-        {
-            public long orderCode { get; set; }
-        }
+
 
         private void BackPressed()
         {
             OnPay = false;
+            
             if (this.PayServiceTask != null)
-                this.PayServiceTask.SetResult(false);
-            OOAdvantech.DeviceApplication.Current.BackPressed-=BackPressed;
+                this.PayServiceTask.SetResult(PaymentPage.PaySucceeded);
+            DeviceApplication.Current.BackPressed-=BackPressed;
 
             Xamarin.Forms.Device.BeginInvokeOnMainThread(async () =>
             {
@@ -108,7 +156,7 @@ namespace OOAdvantech.Pay
         }
     }
 
-    
+
 
 
 }
