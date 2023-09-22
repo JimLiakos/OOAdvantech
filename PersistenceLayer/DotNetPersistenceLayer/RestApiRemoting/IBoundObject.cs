d8f92297d802836b3c5055e8c03acfd6c0ec7af2
Jim Liakos <Jim.Liakos@gmail.com>
2023-09-22 20:03:21 +0300

    jim

--- PersistenceLayer/DotNetPersistenceLayer/RestApiRemoting/IBoundObject.cs
+++ PersistenceLayer/DotNetPersistenceLayer/RestApiRemoting/IBoundObject.cs
@@ -5,6 +5,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#if DeviceDotNet
+using Xamarin.Essentials;
+using MarshalByRefObject = OOAdvantech.Remoting.MarshalByRefObject;
+#else
+using MarshalByRefObject = System.MarshalByRefObject;
+#endif
+
 namespace OOAdvantech.Remoting.RestApi
 {
     /// <MetaDataID>{505ef4bf-5829-4391-8d08-30293a919292}</MetaDataID>
