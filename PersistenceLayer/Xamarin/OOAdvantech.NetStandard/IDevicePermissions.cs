5feab911e5db60bfcc173ffd8b4a3e76bf7ce4f5
Jim Liakos <Jim.Liakos.Msdn@hotmail.com>
2023-09-24 22:50:52 +0300

    jim

--- PersistenceLayer/Xamarin/OOAdvantech.NetStandard/IDevicePermissions.cs
+++ PersistenceLayer/Xamarin/OOAdvantech.NetStandard/IDevicePermissions.cs
@@ -1,14 +1,32 @@
-﻿using System;
+﻿using OOAdvantech.MetaDataRepository;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace OOAdvantech
 {
+
+    /// <MetaDataID>{e47a8e96-0dbb-4db0-9597-4a8f9b3f3536}</MetaDataID>
+    [HttpVisible]
     public interface IDevicePermissions
     {
-        Task<bool> CheckPermissionsForQrcodeScan();
+        Task<bool> CheckPermissionsForQRCodeScan();
+
+
+        /// <summary>
+        /// Request Permission to access infrastructure for QR code scanning 
+        /// </summary>
+        /// <returns>
+        /// for granted  return true
+        /// else return false
+        /// </returns>
         Task<bool> RequestPermissionsForQRCodeScan();
+
+
+        void ShowAppPermissions();
+
+        string DeviceName { get; }
 
     }
 }
