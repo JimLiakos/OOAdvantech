11a5d4746758332b2b96ecb5891077513591b919
Jim Liakos <Jim.Liakos@gmail.com>
2024-02-29 07:42:02 +0200

    jim

--- UserInterface/WPFUserInterfaceLibrary/WPFUIElementObjectBind/DragDropExtension.cs
+++ UserInterface/WPFUserInterfaceLibrary/WPFUIElementObjectBind/DragDropExtension.cs
@@ -122,4 +122,19 @@ namespace WPFUIElementObjectBind
             return null;
         }
     }
+
+    public interface IDragDropTarget
+    {
+        /// <MetaDataID>{3a9c7278-11ab-4c80-a064-b9985297fa73}</MetaDataID>
+        void DragEnter(object sender, DragEventArgs e);
+
+        /// <MetaDataID>{74f60b3c-6346-42f1-9f63-47d3016c18a6}</MetaDataID>
+        void DragLeave(object sender, DragEventArgs e);
+
+        /// <MetaDataID>{2e536b6d-3f44-4c4e-b706-4020a715ff29}</MetaDataID>
+        void DragOver(object sender, DragEventArgs e);
+
+        /// <MetaDataID>{66b60780-81a8-4a4f-a880-c99146d5d54c}</MetaDataID>
+        void Drop(object sender, DragEventArgs e);
+    }
 }
